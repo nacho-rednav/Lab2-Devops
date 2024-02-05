c17217fb3b19617e1d1352e0858fa7db4424336f
JoniSalonen <101516095+JoniSalonen@users.noreply.github.com>
2024-02-05 20:37:02 +0200

    Largest and smallest numbers

--- avg.py
+++ avg.py
@@ -7,3 +7,13 @@ average = (num1 + num2 + num3) / 3
 
 # Display the result
 print("The average of {}, {}, and {} is: {:.2f}".format(num1, num2, num3, average))
+
+def largest_and_smallest(num1, num2, num3):
+    numb = [num1, num2, num3]
+    large = max(numb)
+    small = min(numb)
+    return large, small
+
+large, small = largest_and_smallest(num1, num2, num3)
+print("Largest:", large)
+print("Smallest:", small) 
