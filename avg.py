e9887a4ebc4a719c5ab5a840f970dc3859ddcf35
Nacho Redondo <nacho.redonnav@gmail.com>
2024-02-05 16:34:06 +0000

    compute avg

---
+++ avg.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# Calculate the average
+average = (num1 + num2 + num3) / 3
+
+# Display the result
+print("The average of {}, {}, and {} is: {:.2f}".format(num1, num2, num3, average))
